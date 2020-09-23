dbbd14e1f255cd3d1f1b1aa3494a5d4415c5111f
chrispykan <christineparisk@gmail.com>
2020-09-23 19:21:43 +0200

    defined variables

--- Calculator.java
+++ Calculator.java
@@ -3,12 +3,21 @@ import java.util.*;
 public class Calculator{
    public static void main (String[] args){
    
+      //scanner from java util package to let user input data
       Scanner scan = new Scanner(System.in);
-   
+      //user inputs first integer after system prompt
       System.out.println("\nCalculate any two numbers you want!\n\nEnter first number: ");
-      
+      int num1 = scan.nextInt();
+      //user inputs second integer after system prompt
       System.out.println("Enter second number: ");
-      
+      int num2 = scan.nextInt();
+      //defining operations calculator can perform 
+      int a, s, d, m;
+      a = num1 + num2;
+      s = num1 - num2;
+      d = num1 / num2;
+      m = num1 * num2;
+      //System prompts user to select operation to be performed to the numbers user inputted
       System.out.println( "Do you want to [A] add, [S] subtract, [D] divide or [M] multiply?\n[Q] Quit");   
    }
 }
