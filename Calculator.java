7eaeff145c9daf24fb1c4983b7b117be454df783
chrispykan <christineparisk@gmail.com>
2020-09-23 19:11:24 +0200

    added objectives

--- Calculator.java
+++ Calculator.java
@@ -3,6 +3,12 @@ import java.util.*;
 public class Calculator{
    public static void main (String[] args){
    
-   Scanner scan = new Scanner(System.in);   
+      Scanner scan = new Scanner(System.in);
+   
+      System.out.println("\nCalculate any two numbers you want!\n\nEnter first number: ");
+      
+      System.out.println("Enter second number: ");
+      
+      System.out.println( "Do you want to [A] add, [S] subtract, [D] divide or [M] multiply?\n[Q] Quit");   
    }
 }
