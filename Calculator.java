eccad0bf9452999c2675be6c5b5f60c32622b2d5
chrispykan <christineparisk@gmail.com>
2020-09-23 19:31:04 +0200

    added logic to run according to input

--- Calculator.java
+++ Calculator.java
@@ -18,6 +18,22 @@ public class Calculator{
       d = num1 / num2;
       m = num1 * num2;
       //System prompts user to select operation to be performed to the numbers user inputted
-      System.out.println( "Do you want to [A] add, [S] subtract, [D] divide or [M] multiply?\n[Q] Quit");   
+      System.out.println( "Do you want to [A] add, [S] subtract, [D] divide or [M] multiply?\n[Q] Quit");
+      //user selects operation from options in previous prompt (string)
+      String operation = scan.next();
+   
+      //logic according to user's selected operation
+      if(operation.equals("A")){
+         System.out.println( num1 + " + " + num2 + " = " + a);
+      }
+      else if(operation.equals("S")){
+         System.out.println( num1 + " - " + num2 + " = " + s);
+      }
+      else if(operation.equals("D")){
+         System.out.println( num1 + " / " + num2 + " = " + d);
+      }
+      else{
+         System.out.println( num1 + " * " + num2 + " = " + m);
+      }
    }
 }
